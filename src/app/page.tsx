054ba49dcb2dc152954e87b84d65a1546ec9b2af
M-Araumi <3m5soy2@gmail.com>
2026-06-09 21:50:00 +0900

    mapを使ったメモ一覧表示を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,34 @@
 import Header from "@/components/Header";
 import MemoCard from "@/components/MemoCard";
 export default function Home() {
+  const memos = [
+    {
+      id: 1,
+      title:"買い物",
+      memoText:"牛乳を買う"
+    },
+    {
+      id: 2,
+      title:"仕事",
+      memoText:"週次報告をする"
+    },
+    {
+      id: 3,
+      title:"勉強",
+      memoText:"Reactを学ぶ"
+    }
+  ]
   return (
     <div>
       <Header />
-      <MemoCard
-        title="買い物"
-        memoText="牛乳を買う"
-      />
-      <MemoCard
-        title="仕事"
-        memoText="週次報告をする"
-      />
-      <MemoCard
-        title="勉強"
-        memoText="Reactを学ぶ"
-      />
+      {memos.map((memo) => (
+        <MemoCard
+        key={memo.id}
+        title={memo.title}
+        memoText={memo.memoText}
+        />
+      )
+    )}
     </div>
   );
 }
