bdb001994980fc94835a9db338c963ae81ccbb7b
M-Araumi <3m5soy2@gmail.com>
2026-06-22 23:05:46 +0900

    メモの永続化機能を実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,39 @@ import styles from "./page.module.css"
 import Header from "@/components/Header";
 import MemoForm from "@/components/MemoForm";
 import MemoCard from "@/components/MemoCard";
-import { memo, useState } from "react";
-import { title } from "process";
+import { useState, useEffect } from "react";
 export default function Home() {
   type Memo = {
     id: number;
     title: string;
     memoText: string;
   }
+  const [memos , setMemos] = useState<Memo[]>([])
+
+  useEffect (() => {
+    const data = localStorage.getItem("memos");
+    if(data){
+      setMemos(JSON.parse(data))
+    }
+  },[]);
+
+  useEffect (() => {
+    localStorage.setItem("memos",JSON.stringify(memos));
+  },[memos]);
+
   function handleAddMemo(title:string,memoText:string){
     const newMemo = {
       id: memos.length + 1,
       title: title,
       memoText: memoText
     }
-    setMemos([
-      ...memos,
-      newMemo
-    ]);
+    const updateMemos = [...memos,newMemo]
+    setMemos(updateMemos);
   }
   function handleDelMemo(id:number){
-    setMemos(
-      memos.filter(memo => memo.id !== id)
-    )
+    const deletedMemos = memos.filter(memo => memo.id !== id)
+    setMemos(deletedMemos)
   }
-  const [memos , setMemos] = useState<Memo[]>([])
   return (
     <div className={styles.page}>
       <Header />
