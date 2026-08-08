b529307261d257f84c15b1e5e8f072332b60ade3
M-Araumi <3m5soy2@gmail.com>
2026-08-08 16:04:35 +0900

    LocalStolageの完了状態のチェックと取得エラーハンドリングの追加

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,19 +1,34 @@
 "use client";
 import type { Memo } from "@/types/types";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 export default function useLocalStorage(
     memos: Memo[],
-    setMemos: React.Dispatch<React.SetStateAction<Memo[]>>
+    setMemos: React.Dispatch<React.SetStateAction<Memo[]>>,
+    setErrorMessage: React.Dispatch<React.SetStateAction<string>>
 ){
+    const [isLoaded,setIsLoaded] = useState(false);
     useEffect (() => {
-        const data = localStorage.getItem("memos");
-        if(data){
-            setMemos(JSON.parse(data))
+        try{
+            const data = localStorage.getItem("memos");
+            if(data){
+                setMemos(JSON.parse(data))
+            }
+        }catch(error){
+            if(error instanceof Error){
+                setErrorMessage("メモの取得に失敗しました");
+            }else{
+                setErrorMessage("予期せぬエラーが発生しました")
+            }
+        }finally{
+            setIsLoaded(true);
         }
     },[]);
 
+
     useEffect (() => {
+        if(!isLoaded) return;
+
         localStorage.setItem("memos",JSON.stringify(memos));
 
-    },[memos]);
+    },[memos,isLoaded]);
 };
