a2fd167fd43f860d91fd0c505d55615fa4d7f741
M-Araumi <3m5soy2@gmail.com>
2026-09-10 21:15:24 +0900

    feat: add loading state

--- src/hooks/useMemos.ts
+++ src/hooks/useMemos.ts
@@ -6,19 +6,20 @@ import type { Memo, ApiMemo } from "@/types/types";
 
 export default function useMemos(){
     const [ memos,setMemos ] = useState<Memo[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
     const fetchMemos = async () => {
+        setIsLoading(true);
         const response = await fetch("http://localhost:3000/memos");
         const data: ApiMemo[] = await response.json();
-
         const memos: Memo[] = data.map(memo => ({
             id: memo.id,
             title: memo.title,
             memoText: memo.memo_text
         }));
-
         setMemos(memos);
+        setIsLoading(false);
     };
     fetchMemos();
     }, []);
@@ -26,6 +27,7 @@ export default function useMemos(){
     const [ editingMemo,setEditingMemo ] = useState<Memo|null>(null) 
 
     const addMemo = async(title: string, memoText:string) => {
+        setIsLoading(true);
         const response = await fetch("http://localhost:3000/memos",{
             method: "POST",
             headers: {
@@ -43,9 +45,11 @@ export default function useMemos(){
             memoText: data.memo_text
         };
         setMemos(prev => [...prev, newMemo]);
+        setIsLoading(false);
     }
 
     const updateMemo =async(updatedMemo:Memo) => {
+        setIsLoading(true);
         const response = await fetch(`http://localhost:3000/memos/${updatedMemo.id}`,{
             method: "PATCH",
             headers: {
@@ -64,18 +68,22 @@ export default function useMemos(){
         };
         setMemos(prev => prev.map(memo => memo.id === updateMemo.id ? updateMemo:memo))
         setEditingMemo(null)
+        setIsLoading(false);
     }
 
     const deleteMemo = async(id:string) => {
+        setIsLoading(true);
         const response = await fetch(`http://localhost:3000/memos/${id}`,{
             method: "DELETE"
         });
         setMemos(prev => prev.filter(memo => memo.id !== id))
+        setIsLoading(false);
     }
 
     return{
         memos,
         editingMemo,
+        isLoading,
         setEditingMemo,
         addMemo,
         deleteMemo,
